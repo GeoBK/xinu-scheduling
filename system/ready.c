08fb00665d9a85afa87b6b7edd182f684ede6635
bibin <bibinkurian2@gmail.com>
2019-10-01 13:23:13 -0400

    ct1 final

--- system/ready.c
+++ system/ready.c
@@ -29,11 +29,6 @@ status	ready(
 }
 
 syscall print_ready_list(){	
-	
-	printf("%u\n",queuetail(readylist));
-	printf("%u\n",queuehead(readylist));
-	printf("%u\n",firstid(readylist));
-	printf("%u\n",lastid(readylist));
 	int32 curr;
 	for (curr = firstid(readylist);curr != queuetail(readylist);curr = queuetab[curr].qnext) {
 		printf("PID: %u \n",(unsigned int)curr);		
