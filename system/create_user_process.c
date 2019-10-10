19d42bac46359d2c6f1efa6793c091f531ff926a
bibin <bibinkurian2@gmail.com>
2019-10-09 22:35:48 -0400

    error free

--- system/create_user_process.c
+++ system/create_user_process.c
@@ -8,7 +8,7 @@ local	int newpid();
  *  create_user_process  -  Create a user process to start running a function on x86
  *------------------------------------------------------------------------
  */
-pid32	create_user_process(
+ pid32	create_user_process(
 	  void		*funcaddr,	/* Address of the function	*/
 	  uint32	ssize,		/* Stack size in bytes		*/	  
 	  char		*name,		/* Name (for debugging)		*/
@@ -18,12 +18,13 @@ pid32	create_user_process(
 {
     
     pri16       priority=1;
-    uint32		*a;		/* Points to list of args	*/
-    /* Push arguments */
-	a = (uint32 *)(&nargs + 1);	/* Start of args		*/
-	a += nargs -1;			/* Last argument		*/
-	for ( ; nargs > 0 ; nargs--)	/* Machine dependent; copy args	*/
-		*--saddr = *a--;	/* onto created process's stack	*/
-	*--saddr = (long)INITRET;	/* Push on return address	*/
-	return create(funcaddr,ssize,priority,name,nargs)
+    // uint32		*a;		/* Points to list of args	*/
+    // /* Push arguments */
+	// a = (uint32 *)(&nargs + 1);	/* Start of args		*/
+	// a += nargs -1;			/* Last argument		*/
+    // asm()
+	// for ( ; nargs > 0 ; nargs--)	/* Machine dependent; copy args	*/
+	// 	*--saddr = *a--;	/* onto created process's stack	*/
+	// *--saddr = (long)INITRET;	/* Push on return address	*/
+	return create(funcaddr,ssize,priority,name,nargs);
 }
