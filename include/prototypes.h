1e6d09b8f4c9904031b924e213b97e7395907a1c
bibin <bibinkurian2@gmail.com>
2019-10-10 01:46:08 -0400

    warningfix

--- include/prototypes.h
+++ include/prototypes.h
@@ -616,6 +616,8 @@ extern void burst_execution(uint32 number_bursts, uint32 burst_duration, uint32 
 /**/
 extern void set_tickets(pid32 pid, uint32 tickets); 
 
+extern	pid32	create_user_process(void *, uint32, char *, uint32, ...);
+
 /* NETWORK BYTE ORDER CONVERSION NOT NEEDED ON A BIG-ENDIAN COMPUTER */
 #define	htons(x)  ((0xff & ((x)>>8)) | ((0xff & (x)) << 8))
 #define	htonl(x)  ((((x)>>24) & 0x000000ff) | (((x)>> 8) & 0x0000ff00) | \
