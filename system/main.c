d43adb4c16ec8272a482dbd588226457f042cce7
bibin <bibinkurian2@gmail.com>
2019-10-10 18:58:39 -0400

    test run

--- system/main.c
+++ system/main.c
@@ -1,6 +1,6 @@
 /*  main.c  - main */
 
-#include <xinu.h>
+// #include <xinu.h>
 
 // process	main(void)
 // {
@@ -22,24 +22,93 @@
     
 // }
 
+// void sync_printf(char *fmt, ...)
+// {
+//     	intmask mask = disable();
+// 	void *arg = __builtin_apply_args();
+// 	__builtin_apply((void*)kprintf, arg, 100);
+// 	restore(mask);
+// }
+// void test1(uint32 a){
+// 	printf("test1 : %d\n",a);
+// }
+// process	main(void)
+// {    
+	
+// 	pid32 pid1=create_user_process((void *)test1, 8192, "test1", 1, 123);	
+// 	resume(pid1);
+// 	print_ready_list();	
+// 	set_tickets(pid1,50);	
+// 	//burst_execution(1,10,10);
+// 	return OK;
+    
+// }
+
+
+
+
+#include <xinu.h>
+#include <stdio.h>
+
+void timed_execution(uint32 runtime){
+	    while(proctab[currpid].runtime<runtime);
+}
+
 void sync_printf(char *fmt, ...)
 {
-    	intmask mask = disable();
-	void *arg = __builtin_apply_args();
-	__builtin_apply((void*)kprintf, arg, 100);
-	restore(mask);
+        intmask mask = disable();
+        void *arg = __builtin_apply_args();
+        __builtin_apply((void*)kprintf, arg, 100);
+        restore(mask);
 }
-void test1(uint32 a){
-	printf("test1 : %d\n",a);
-}
-process	main(void)
-{    
+
+int main() {
+	pid32 prA, prB, prC;
+
+	sync_printf("\n");
+
+	sync_printf("=== TESTCASE 1::  1 process - context switches =================\n");	
+	prA = create_user_process(timed_execution, 1024, "timed_execution", 1, 100);
+	set_tickets(prA, 50);
+	resume(prA);
+	receive();
+	sleepms(50); //wait for user process to terminate	
+	kprintf("\nprocess %d:: runtime=%d, turnaround time=%d, ctx=%d\n",prA, proctab[prA].runtime, proctab[prA].turnaroundtime, proctab[prA].num_ctxsw);
 	
-	pid32 pid1=create_user_process((void *)test1, 8192, "test1", 1, 123);	
-	resume(pid1);
-	print_ready_list();	
-	set_tickets(pid1,50);	
-	//burst_execution(1,10,10);
+	// sync_printf("================================================================\n\n");	
+	// sync_printf("=== TESTCASE 2::  2 processes - context switches ===============\n");	
+	// prA = create_user_process(timed_execution, 1024, "timed_execution", 1, 100);
+	// prB = create_user_process(timed_execution, 1024, "timed_execution", 1, 100);
+	// set_tickets(prA, 50);
+	// set_tickets(prB, 50);
+	// resume(prA);
+	// resume(prB);
+	// receive();
+	// receive();
+	// sleepms(50); //wait for user processes to terminate	
+	// kprintf("\nprocess %d:: runtime=%d, turnaround time=%d, ctx=%d\n",prA, proctab[prA].runtime, proctab[prA].turnaroundtime, proctab[prA].num_ctxsw);
+    //     kprintf("process %d:: runtime=%d, turnaround time=%d, ctx=%d\n",prB, proctab[prB].runtime, proctab[prB].turnaroundtime, proctab[prB].num_ctxsw);
+	
+	// sync_printf("================================================================\n\n");	
+
+	// sync_printf("=== TESTCASE 3::  3 processes - context switches ===============\n");	
+	// prA = create_user_process(timed_execution, 1024, "timed_execution", 1, 100);
+	// prB = create_user_process(timed_execution, 1024, "timed_execution", 1, 100);
+	// prC = create_user_process(timed_execution, 1024, "timed_execution", 1, 40);
+	// set_tickets(prA, 50);
+	// set_tickets(prB, 50);
+	// set_tickets(prC, 50);
+	// resume(prA);
+	// resume(prB);
+	// sleepms(50);	
+	// resume(prC);
+	// receive();
+	// receive();
+	// receive();
+	// sleepms(50); //wait for user processes to terminate	
+	// kprintf("\nprocess %d:: runtime=%d, turnaround time=%d, ctx=%d\n",prA, proctab[prA].runtime, proctab[prA].turnaroundtime, proctab[prA].num_ctxsw);
+    //     kprintf("process %d:: runtime=%d, turnaround time=%d, ctx=%d\n",prB, proctab[prB].runtime, proctab[prB].turnaroundtime, proctab[prB].num_ctxsw);
+    //     kprintf("process %d:: runtime=%d, turnaround time=%d, ctx=%d\n",prC, proctab[prC].runtime, proctab[prC].turnaroundtime, proctab[prC].num_ctxsw);
+	// sync_printf("================================================================\n\n");	
 	return OK;
-    
 }
