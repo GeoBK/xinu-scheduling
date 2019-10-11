cd1378f7279a8a720d0ddde71b20998f3da951ed
bibin <bibinkurian2@gmail.com>
2019-10-11 00:05:43 -0400

    test

--- system/burst_execution.c
+++ system/burst_execution.c
@@ -6,7 +6,7 @@ void burst_execution(uint32 number_bursts, uint32 burst_duration, uint32 sleep_d
     for(i=0;i<number_bursts;i++)
     {
         int stime = ctr1000;
-        while((ctr1000-stime)<burst_duration);        
+        while(proctab[currpid].runtime+ctr1000-proctab[currpid].runstime<burst_duration);
         sleepms(sleep_duration);
     }
 
