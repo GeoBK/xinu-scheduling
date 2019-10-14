2b2de09768662d179f0229d0f966dd12fd313514
bibin <bibinkurian2@gmail.com>
2019-10-13 23:04:36 -0400

    des

--- system/main.c
+++ system/main.c
@@ -22,60 +22,60 @@ void sync_printf(char *fmt, ...)
 int main() {
 	pid32 prA, prB, prC, prD;
 
-	// kprintf("\n");
+	kprintf("\n");
 
-	// kprintf("QUANTUM=%d, TIME_ALLOTMENT=%d, PRIORITY_BOOST_PERIOD=%d\n\n", QUANTUM, TIME_ALLOTMENT, PRIORITY_BOOST_PERIOD);
+	kprintf("QUANTUM=%d, TIME_ALLOTMENT=%d, PRIORITY_BOOST_PERIOD=%d\n\n", QUANTUM, TIME_ALLOTMENT, PRIORITY_BOOST_PERIOD);
 	
-	// kprintf("=== TESTCASE 1::  CPU-intensive jobs =============================\n");
+	kprintf("=== TESTCASE 1::  CPU-intensive jobs =============================\n");
 
-	// prA = create_user_process(timed_execution, 1024, "cpu_1000", 1, 1000);
-	// prB = create_user_process(timed_execution, 1024, "cpu_1000", 1, 1000);
+	prA = create_user_process(timed_execution, 1024, "cpu_1000", 1, 1000);
+	prB = create_user_process(timed_execution, 1024, "cpu_1000", 1, 1000);
 
-	// resume(prA);
-	// resume(prB);
-	// //print_ready_list();
+	resume(prA);
+	resume(prB);
+	//print_ready_list();
 
-	// receive();	
-	// receive();	
+	receive();	
+	receive();	
 
-	// sleepms(50); // wait for user processes to terminate	
+	sleepms(50); // wait for user processes to terminate	
 
-	// sync_printf("reset_counter : %d\n",reset_counter);
+	sync_printf("reset_counter : %d\n",reset_counter);
 
-	// kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prA, proctab[prA].prname, proctab[prA].runtime, proctab[prA].turnaroundtime, proctab[prA].num_ctxsw);
-	// kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prB, proctab[prB].prname, proctab[prB].runtime, proctab[prB].turnaroundtime, proctab[prB].num_ctxsw);
+	kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prA, proctab[prA].prname, proctab[prA].runtime, proctab[prA].turnaroundtime, proctab[prA].num_ctxsw);
+	kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prB, proctab[prB].prname, proctab[prB].runtime, proctab[prB].turnaroundtime, proctab[prB].num_ctxsw);
 
-	// kprintf("==================================================================\n\n");
+	kprintf("==================================================================\n\n");
 
-    //     kprintf("=== TESTCASE 2::  CPU-intensive jobs =============================\n");
+        kprintf("=== TESTCASE 2::  CPU-intensive jobs =============================\n");
 
-    //     prA = create_user_process(timed_execution, 1024, "cpu_1000", 1, 1000);
-    //     prB = create_user_process(timed_execution, 1024, "cpu_1000", 1, 1000);
-    //     prC = create_user_process(timed_execution, 1024, "cpu_1000_new", 1, 1000);
-    //     prD = create_user_process(timed_execution, 1024, "cpu_1000_del", 1, 1000);
+        prA = create_user_process(timed_execution, 1024, "cpu_1000", 1, 1000);
+        prB = create_user_process(timed_execution, 1024, "cpu_1000", 1, 1000);
+        prC = create_user_process(timed_execution, 1024, "cpu_1000_new", 1, 1000);
+        prD = create_user_process(timed_execution, 1024, "cpu_1000_del", 1, 1000);
 
-    //     resume(prA);
-    //     resume(prB);
+        resume(prA);
+        resume(prB);
         
-	// receive();
-	// receive();
+	receive();
+	receive();
 
-	// resume(prC);
+	resume(prC);
 
-	// sleepms(500);
+	sleepms(500);
 
-    //     resume(prD);
+        resume(prD);
 
-    //     receive();
-    //     receive();
+        receive();
+        receive();
 
-    //     sleepms(50); // wait for user processes to terminate    
-	// 	sync_printf("reset_counter : %d\n",reset_counter);
+        sleepms(50); // wait for user processes to terminate    
+		sync_printf("reset_counter : %d\n",reset_counter);
 
-    //     kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prA, proctab[prA].prname, proctab[prA].runtime, proctab[prA].turnaroundtime, proctab[prA].num_ctxsw);
-    //     kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prB, proctab[prB].prname, proctab[prB].runtime, proctab[prB].turnaroundtime, proctab[prB].num_ctxsw);
-    //     kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prC, proctab[prC].prname, proctab[prC].runtime, proctab[prC].turnaroundtime, proctab[prC].num_ctxsw);
-    //     kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prD, proctab[prD].prname, proctab[prD].runtime, proctab[prD].turnaroundtime, proctab[prD].num_ctxsw);
+        kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prA, proctab[prA].prname, proctab[prA].runtime, proctab[prA].turnaroundtime, proctab[prA].num_ctxsw);
+        kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prB, proctab[prB].prname, proctab[prB].runtime, proctab[prB].turnaroundtime, proctab[prB].num_ctxsw);
+        kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prC, proctab[prC].prname, proctab[prC].runtime, proctab[prC].turnaroundtime, proctab[prC].num_ctxsw);
+        kprintf("process %d:: name=%s, runtime=%d, turnaround time=%d, ctx=%d\n",prD, proctab[prD].prname, proctab[prD].runtime, proctab[prD].turnaroundtime, proctab[prD].num_ctxsw);
 
         kprintf("==================================================================\n\n");
 
